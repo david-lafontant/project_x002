a7392313bdb43af67a3603c34ed9442966231d32
david-lafontant <lafontant.david@gmail.com>
2025-01-29 15:23:06 -0500

    Auto fix ruby error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,16 +87,15 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # Disable Rails's static asset server (Apache or nginx will already do this)  
+  # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS  
+  # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed  
+  # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  # Generate digests for assets URLs  
+  # Generate digests for assets URLs
   config.assets.digest = true
-
 end
