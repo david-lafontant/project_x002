3f1efcc798b6e5b705106df2e14abdc3ba601570
david-lafontant <lafontant.david@gmail.com>
2025-02-02 14:26:04 -0500

    Scaffold category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_185648) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_02_191327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_30_185648) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
