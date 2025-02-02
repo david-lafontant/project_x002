e909d95c86f98e7bc6312740f49c8f61bc7b2e94
david-lafontant <lafontant.david@gmail.com>
2025-02-02 12:28:39 -0500

    Update article bot with valid attributes

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    subtitle { "MyString" }
-    content { nil }
+    title { "Lorem ipsum dolor sim amet consectetur" }
+    subtitle { "Lorem ipsum dolor sit amet" }
+    content { "<h1>Hello world</h1>
+    <img src='https://picsum.photos/200' alt='image' />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc egestas mauris quis mi dignissim 
+    euismod. Nullam commodo elementum odio, vitae pellentesque purus convallis non.</p>" }
   end
 end
